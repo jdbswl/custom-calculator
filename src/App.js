a3b8e37e5dac80a9005fe252b43a3eef3e1746b3
Jonathan Ballone <jonathan@scratchworklabs.com>
2019-03-10 13:20:50 -0400

    Add and List Orders works

--- src/App.js
+++ src/App.js
@@ -2,21 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Amplify from 'aws-amplify';
+import Amplify, { Analytics, API, graphqlOperation} from 'aws-amplify';
 import awsmobile from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
 Amplify.configure(awsmobile);
 
+const listOrders = `query listOrders {
+  listOrders{
+    items{
+      id
+      name
+    }
+  }
+}`
+
+const addOrder = `mutation createOrder($name:String!) {
+  createOrder(input:{
+    name:$name
+  }){
+    id
+    name
+  }
+}`
+
+
 class App extends Component {
+  orderMutation = async () => {
+    const orderDetails = {
+      name: 'New order'
+    };
+    const newEvent = await API.graphql(graphqlOperation(addOrder, orderDetails));
+    alert(JSON.stringify(newEvent));
+  }
+
+  listQuery = async () => {
+    console.log('listing orders');
+    const allOrders = await API.graphql(graphqlOperation(listOrders));
+    alert(JSON.stringify(allOrders));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.orderMutation}>GraphQL Mutation</button>
       </div>
     );
   }
