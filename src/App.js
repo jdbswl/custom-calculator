ba266cc96360ad500032ebd6fa902e4c2b5f0e40
Jonathan Ballone <jonathan@scratchworklabs.com>
2019-03-10 15:31:06 -0400

    Wrapped AppSyncClient in Auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import AWSAppSyncClient from 'aws-appsync';
+import { Rehydrated } from 'aws-appsync-react';
+import { ApolloProvider } from 'react-apollo';
 import React, { Component } from 'react';
 import './App.css';
-import Amplify, { graphqlOperation} from 'aws-amplify';
+import Amplify, { Auth, graphqlOperation} from 'aws-amplify';
 import { Connect } from 'aws-amplify-react';
 import awsmobile from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
@@ -14,30 +17,43 @@ import * as subscriptions from './graphql/subscriptions';
 
 Amplify.configure(awsmobile);
 
+const client = new AWSAppSyncClient({
+  url: awsmobile.aws_appsync_graphqlEndpoint,
+  region: awsmobile.aws_appsync_region,
+  auth: {
+    type: awsmobile.aws_appsync_authenticationType,
+    jwtToken: async () => (await Auth.currenSession()).getAccessToken().getJwtToken(),
+  }
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Connect mutation={graphqlOperation(mutations.createOrder)}>
-          {({mutation}) => (
-            <MinimalAddOrder onCreate={mutation} />
-          )}
-        </Connect>
+      <ApolloProvider client={client}>
+        <Rehydrated>
+          <div className="App">
+            <Connect mutation={graphqlOperation(mutations.createOrder)}>
+              {({mutation}) => (
+                <MinimalAddOrder onCreate={mutation} />
+              )}
+            </Connect>
 
-        <Connect query={graphqlOperation(queries.listOrders)}
-          subscription={graphqlOperation(subscriptions.onCreateOrder)}
-          onSubscriptionMsg={(prev, {onCreateOrder}) => {
-            console.log('Order data: ', onCreateOrder);
-            return prev;
-          }
-        }>
-          {({ data: { listOrders }, loading, error }) => {
-            if(error) return (<h3>Error</h3>);
-            if(loading || !listOrders) return (<h3>Loading...</h3>);
-            return (<MinimalOrderList orders={listOrders.items} />);
-          }}
-        </Connect>
-      </div>
+            <Connect query={graphqlOperation(queries.listOrders)}
+              subscription={graphqlOperation(subscriptions.onCreateOrder)}
+              onSubscriptionMsg={(prev, {onCreateOrder}) => {
+                console.log('Order data: ', onCreateOrder);
+                return prev;
+              }
+            }>
+              {({ data: { listOrders }, loading, error }) => {
+                if(error) return (<h3>Error</h3>);
+                if(loading || !listOrders) return (<h3>Loading...</h3>);
+                return (<MinimalOrderList orders={listOrders.items} />);
+              }}
+            </Connect>
+          </div>
+        </Rehydrated>
+      </ApolloProvider>
     );
   }
 }
