7ce071870a5d2c57ff83aaded41c086f9e0d904d
Jonathan Ballone <jonathan@scratchworklabs.com>
2019-03-24 12:15:07 -0400

    Order List works

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -4,17 +4,34 @@ import gql from 'graphql-tag';
 
 import * as queries from '../graphql/queries';
 
+const LIST_ORDERS = gql(queries.listOrders);
+
 const OrderList = () => (
-  <Query
-    query={gql(queries.listOrders)}
-  >
+  <Query query={LIST_ORDERS}>
     {({ loading, error, data }) => {
       if(loading) return (<p>Loading...</p>);
       if(error) return (<p>Error</p>);
-
-      return (<p>here</p>);
+      return data.listOrders.items.map(({id, name}) => (
+        <div key={id}>
+          <p>{name}</p>
+        </div>
+      ));
     }}
   </Query>
 )
 
 export default OrderList;
+
+      // return (
+      //   <div>
+      //     <p>Still here....</p>
+      //     <p>{data.listOrders.items.length}</p>
+      //   </div>
+      // );
+
+
+      // return data.listOrders.items.map((item) => {
+      //   <div>
+      //     <p>{item.name}</p>
+      //   </div>
+      // })
