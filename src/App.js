f6ef51cf3e72c38413c460ad9e4795680003163f
Jonathan Ballone <jonathan@scratchworklabs.com>
2019-04-06 10:12:33 -0400

    Removed Phone Number field from Sign Up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import Amplify from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react';
-import AWSAppSyncClient from 'aws-appsync';
-import { Rehydrated } from 'aws-appsync-react';
-import React from 'react';
-import { ApolloProvider } from 'react-apollo';
+import Amplify from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react'
+import AWSAppSyncClient from 'aws-appsync'
+import { Rehydrated } from 'aws-appsync-react'
+import React from 'react'
+import { ApolloProvider } from 'react-apollo'
 
-import './App.css';
-import awsmobile from './aws-exports';
-import AppRouter from './AppRouter';
+import './App.css'
+import awsmobile from './aws-exports'
+import AppRouter from './AppRouter'
 
 
-Amplify.configure(awsmobile);
+Amplify.configure(awsmobile)
+
 
 class App extends React.Component {
   render() {
-    console.log('App props:', this.props);
-    const signInUserSession = this.props.authData.signInUserSession;
-    let accessToken = null;
+    console.log('App Component props:', this.props)
+    const signInUserSession = this.props.authData.signInUserSession
+    let accessToken = null
     if(signInUserSession) {
-      accessToken = signInUserSession.accessToken.jwtToken;
+      accessToken = signInUserSession.accessToken.jwtToken
     }
 
     const client = new AWSAppSyncClient({
@@ -28,7 +29,7 @@ class App extends React.Component {
         type: awsmobile.aws_appsync_authenticationType,
         jwtToken: accessToken
       }
-    });
+    })
 
     return (
       <ApolloProvider client={client}>
@@ -40,74 +41,10 @@ class App extends React.Component {
   }
 }
 
-const signUpConfig = {
+
+export default withAuthenticator(App, {
   includeGreetings: true,
-  hiddenDefaults: ['phone_number'],
-}
-
-
-
-
-
-
-
-// const ApolloRoutes = compose(
-//   graphql(gql(queries.listOrders), {
-//       options: {
-//         fetchPolicy: 'cache-and-network'
-//       },
-//       props: props => ({
-//         orders: props.data.listOrders ? props.data.listOrders.items : []
-//       })
-//     })
-// )(AppRouter)
-
-    // return (
-    //   <ApolloProvider client={client}>
-    //     <Rehydrated>
-    //       <ApolloRoutes />
-    //     </Rehydrated>
-    //   </ApolloProvider>
-    // );
-
-
-
-
-
-
-// class AppWithAuthenticator extends React.Component {
-//   render() {
-//     return withAuthenticator(App, {
-//       signUpConfig
-//     });
-//   }
-// }
-
-export default withAuthenticator(App, signUpConfig);
-
-
-
-//
-//
-//
-// <div className="App">
-//   <Connect mutation={graphqlOperation(mutations.createOrder)}>
-//     {({mutation}) => (
-//       <MinimalAddOrder onCreate={mutation} />
-//     )}
-//   </Connect>
-//
-//   <Connect query={graphqlOperation(queries.listOrders)}
-//     subscription={graphqlOperation(subscriptions.onCreateOrder)}
-//     onSubscriptionMsg={(prev, {onCreateOrder}) => {
-//       console.log('Order data: ', onCreateOrder);
-//       return prev;
-//     }
-//   }>
-//     {({ data: { listOrders }, loading, error }) => {
-//       if(error) return (<h3>Error</h3>);
-//       if(loading || !listOrders) return (<h3>Loading...</h3>);
-//       return (<MinimalOrderList orders={listOrders.items} />);
-//     }}
-//   </Connect>
-// </div>
+  signUpConfig: {
+    hiddenDefaults: ['phone_number']
+  }
+})
