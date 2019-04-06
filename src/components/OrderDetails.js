e6a86b569ca7f13f1ffe33680f45dc8f83d3cd17
Jonathan Ballone <jonathan@scratchworklabs.com>
2019-04-06 12:17:36 -0400

    Added Update button to Order Detail Component

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -15,16 +15,18 @@ import * as mutations from '../graphql/mutations'
 import * as queries from '../graphql/queries'
 
 
-const DELETE_ORDER = gql(mutations.deleteOrder)
 const GET_ORDER = gql(queries.getOrder)
+const DELETE_ORDER = gql(mutations.deleteOrder)
+const UPDATE_ORDER = gql(mutations.updateOrder)
+
 
 class OrderDetails extends React.Component {
   constructor(props) {
     super(props)
-    this.routeChange = this.routeChange.bind(this)
+    this.backToOrderList = this.backToOrderList.bind(this)
   }
 
-  routeChange() {
+  backToOrderList() {
     let path = `/order-list`
     this.props.history.push(path)
   }
@@ -32,40 +34,71 @@ class OrderDetails extends React.Component {
   render() {
     return (
       <div>
-        <Query query={GET_ORDER} variables={{id:this.props.match.params.id}}>
-          {({ loading, error, data }) => {
-            if(loading) {
-              return (<p>Loading...</p>)
-            } else if(error) {
-              console.log(error)
-              return (<p>Error</p>)
-            } else {
-              return (
-                <div>
-                  <h2>Order Details</h2>
+        <Query
+          query={GET_ORDER}
+          variables={{id:this.props.match.params.id}}
+          pollInterval={500}>
+            {({ loading, error, data }) => {
+              if(loading) {
+                return (<p>Loading...</p>)
+              } else if(error) {
+                console.log(error)
+                return (<p>Error</p>)
+              } else {
+                return (
+                  <div>
+                    <h2>Order Details</h2>
                     <div>
-                      <p>Name: {data.getOrder.name}</p>
-                      <p>Link: /order-details/{data.getOrder.id} (todo: copy-to-clipboard button)</p>
-                      <p>Notes: {data.getOrder.notes ? data.getOrder.notes : 'No Notes'}</p>
-                      <p>List Items: {data.getOrder.listItems ? data.getOrder.listItems : 'No List Items'}</p>
+                       <p>{data.getOrder.name}</p>
                     </div>
-                  <Mutation mutation={DELETE_ORDER}>
-                  {(deleteOrder, {data}) => (
-                    <div>
-                      <button onClick={e => {
-                        deleteOrder({ variables: { input: {
-                          id: this.props.match.params.id
-                        }}})
-                        this.routeChange()
-                      }}
-                      >Delete</button>
-                    </div>
-                  )}
-                  </Mutation>
-                </div>
-              )
-            }
-          }}
+
+                    <Mutation mutation={UPDATE_ORDER}>
+                    {(updateOrder, {data}) => (
+                      <div>
+                        <button onClick={e => {
+                          updateOrder({
+                            variables: {
+                              input: {
+                                id: 'be7e4149-54f9-46b3-bf72-cab07176e216',
+                                name: 'Updated'
+                              }
+                            }
+                          })
+                        }}>Update</button>
+                      </div>
+                    )}
+                    </Mutation>
+
+                    <Mutation mutation={DELETE_ORDER}>
+                    {(deleteOrder, {data}) => (
+                      <div>
+                        <button onClick={e => {
+                          deleteOrder({ variables: { input: {
+                            id: this.props.match.params.id
+                          }}})
+                          this.backToOrderList()
+                        }}>
+                          Delete
+                        </button>
+                      </div>
+                    )}
+                    </Mutation>
+                  </div>
+                )
+                // return (
+                //   <div>
+                //     <h2>Order Details</h2>
+                //       <div>
+
+                //         <p>Link: /order-details/{data.getOrder.id} (todo: copy-to-clipboard button)</p>
+
+                //         <p>Line Items: {data.getOrder.lineItems ? data.getOrder.lineItems : 'No Line Items'}</p>
+                //       </div>
+
+                //   </div>
+                // )
+              }
+            }}
         </Query>
       </div>
     )
@@ -73,25 +106,4 @@ class OrderDetails extends React.Component {
 }
 
 
-export default OrderDetails
-
-
-
-
-
-// data.listOrders.items.map((order, index) => (
-//   <ListItem key={order.id}>
-//     <ListItemText primary={order.name} secondary={order.id}/>
-//     <ListItemLink>Edit</ListItemLink>
-//   </ListItem>
-// ))
-// return data.listOrders.items.map((order, index) => (
-//   <div key={index}>
-//     <p>{index+1}. {order.name}  <Button variant="outlined" color="primary">Edit</Button></p>
-//   </div>
-// ))
-      // return data.listOrders.items.map((order, index) => (
-      //   <div key={index}>
-      //     <p>{index+1}. {order.name}  <Button variant="outlined" color="primary">Edit</Button></p>
-      //   </div>
-      // ))
+export default withRouter(OrderDetails)
