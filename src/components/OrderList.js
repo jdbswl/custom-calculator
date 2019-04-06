da9ed0a80c0b5d2df0600bc0474772f393402ab2
Jonathan Ballone <jonathan@scratchworklabs.com>
2019-04-06 10:18:14 -0400

    Converted Order List to a minimal component as opposed to a simple render function

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,56 +1,72 @@
-import React from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import { withStyles } from '@material-ui/core/styles';
+import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import { withStyles } from '@material-ui/core/styles'
 // import Button from '@material-ui/core/Button';
 // import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 
-import QuickAddOrder from './QuickAddOrder';
-import * as queries from '../graphql/queries';
 
-const LIST_ORDERS = gql(queries.listOrders);
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import QuickAddOrder from './QuickAddOrder'
+import * as queries from '../graphql/queries'
+
+
+
+const LIST_ORDERS = gql(queries.listOrders)
+
+
 
 const styles = theme => ({
   root: {
     width: '100%',
     maxWidth: 360,
-    // backgroundColor: theme.pallete.background.paper,
   },
-});
+})
+
 
 function ListItemLink(props) {
-  return <ListItem button component="a" {...props}/>;
+  return <ListItem button component="a" {...props}/>
 }
 
-const OrderList = () => (
-  <div>
-    <QuickAddOrder />
-    <Query query={LIST_ORDERS}>
-      {({ loading, error, data }) => {
-        if(loading) return (<p>Loading...</p>);
-        if(error) return (<p>Error</p>);
-        return (
-          <div>
-            <h2>Orders</h2>
-            <List>
-              {
-                data.listOrders.items.map((order, index) => (
-                  <ListItem key={order.id}>
-                    <ListItemText primary={order.name} secondary={order.id}/>
-                    <ListItemLink href={'/order-details/' + order.id}>Details</ListItemLink>
-                  </ListItem>
-                ))
-              }
-            </List>
-          </div>
-        );
-      }}
-    </Query>
-  </div>
-)
+
+class OrderList extends React.Component {
+  render() {
+    console.log('Order List Component props:', this.props)
+    return (
+      <h1>My Order List</h1>
+    )
+  }
+}
+
+
+// const OrderList = () => (
+//   <div>
+//     <QuickAddOrder />
+//     <Query query={LIST_ORDERS}>
+//       {({ loading, error, data }) => {
+//         if(loading) return (<p>Loading...</p>);
+//         if(error) return (<p>Error</p>);
+//         return (
+//           <div>
+//             <h2>Orders</h2>
+//             <List>
+//               {
+//                 data.listOrders.items.map((order, index) => (
+//                   <ListItem key={order.id}>
+//                     <ListItemText primary={order.name} secondary={order.id}/>
+//                     <ListItemLink href={'/order-details/' + order.id}>Details</ListItemLink>
+//                   </ListItem>
+//                 ))
+//               }
+//             </List>
+//           </div>
+//         );
+//       }}
+//     </Query>
+//   </div>
+// )
 
 export default withStyles(styles)(OrderList);
 
