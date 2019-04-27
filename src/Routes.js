635dfb190d4e797cbecec3eeb80edd178eb00726
Jonathan Ballone <jonathan@scratchworklabs.com>
2019-04-27 10:17:24 -0400

    X Marks the spot

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,3 @@
-import { Home, ContentPaste, Notifications, AccountCircle } from '@material-ui/icons'
-
 import OrderList from './components/OrderList'
 import AddOrder from './components/AddOrder'
 
@@ -8,13 +6,11 @@ const Routes = [
     path: '/order-list/',
     sidebarName: 'Orders',
     navbarName: 'Orders',
-    icon: null,
     component: OrderList
   }, {
     path: '/add-order/',
     sidebarName: 'Add Order',
     navbarName: 'Add Order',
-    icon: null,
     component: AddOrder
   }
 ]
